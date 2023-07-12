95bf629c9d44caa1eefd675bb5df90a654fe82ce
irvands <yaelahvannn.id@gmail.com>
2023-07-12 19:39:45 +0700

    install and integrate socialite

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,7 @@ class User extends Authenticatable
         'avatar',
         'is_admmin',
         'occupation',
+        'email_verified_at'
 
     ];
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,4 +29,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
+//socialite routes
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('/auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 require __DIR__.'/auth.php';
